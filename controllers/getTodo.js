6b6ce48a900e9f3514eebe1c7be563c9b24d0cfb
Subrata Jana <subratajana7735@gmail.com>
2026-04-01 20:08:41 +0530

    Get todo routes

---
+++ controllers/getTodo.js
@@ -0,0 +1,26 @@
+
+
+const Todo = require('../models/Todo');
+
+exports.getTodo = async (req, res) => {
+    try {
+
+        // fetch All data
+        const AllTodos = await Todo.find({});
+        // send response
+        res.status(200).json({
+            success: true,
+            data: AllTodos,
+            message: "Fetch Data successfull"
+        })
+
+    } catch (e) {
+        console.error(e);
+        res.status(500).json({
+            success: false,
+            data: e,
+            message: "Server Error"
+        })
+
+    }
+};
--- routes/Todos.js
+++ routes/Todos.js
@@ -4,9 +4,18 @@ const express = require('express');
 const Router = express.Router();
 // import Controller
 const { createTodo } = require('../controllers/createTodo');
+const { getTodo } = require('../controllers/getTodo');
 // define API
 // create Route so use post req 
 // build mapping  between route and controllers
+
+
+// Get request
+Router.get('/getTodo', getTodo);
+// post request
 Router.post('/createTodoList', createTodo);
+
+
+
 module.exports = Router;
 
